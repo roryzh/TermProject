0619e56057f447b15281452b203e053bde7d9c11
unknown <tandoni@TANDONI-1.rose-hulman.edu>
2015-02-12 15:49:36 -0500

    gui update

--- GUI/src/MapFrame.java
+++ GUI/src/MapFrame.java
@@ -1,15 +1,36 @@
-import java.awt.*;
-
-import javax.swing.*;
-import javax.swing.border.TitledBorder;
-
-import java.awt.event.*;
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Ellipse2D;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ListCellRenderer;
+import javax.swing.border.TitledBorder;
 
 /**
  * 
@@ -21,11 +42,13 @@ public class MapFrame extends JFrame {
 	static ArrayList<String> places = null;
 	private static final int WIDTH = 1000;
 	private static final int HEIGHT = 1000;
-	public Panel panel = new Panel();
-	public static Console console;// = new Console();
+	public Panel panel;
+	public Console console= new Console();
+	static HashMap<String, String> cities = new HashMap<String, String>();
 
 	public MapFrame() {
 		super("MAPS");
+		panel = new Panel(cities);
 		this.setSize(WIDTH, HEIGHT);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container content = getContentPane();
@@ -42,35 +65,22 @@ public class MapFrame extends JFrame {
 
 	public static void main(String[] args) {
 		places = new ArrayList<String>();
-		// JButton button = new JButton();
-		// ArrayList<JButton> buttons = new ArrayList<JButton>();
-		// JPanel panel = new JPanel();
-		// JFrame buttonFrame = new JFrame();
-		// buttonFrame.setAlwaysOnTop(true);
-		// JTextField whereToStart = new JTextField();
-		// buttonFrame.add(whereToStart);
-		//
-		// buttonFrame.add(panel);
-		// buttonFrame.setSize(WIDTH, HEIGHT);
-		// buttonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// buttonFrame.setVisible(true);
-		// panel.setVisible(true);
-
-		// places.add("asdasdas");
 		try {
 			BufferedReader read = new BufferedReader(new FileReader("input.in"));
-			String line = read.readLine().toLowerCase();
-			
-			
-			
-			
+			int numberOfCities = Integer.parseInt(read.readLine().toString());
+			for (int i = 0; i < numberOfCities; i++) {
+				cities.put(read.readLine().toString(), read.readLine()
+						.toString());
+			}
+
 		} catch (IOException error) {
-			MapFrame.console.setText("File not Found");
+			//this.console.setText("File not Found");
 		}
-		// places.add("Indianapolis");
-		// places.add("Terre Haute");
-		// places.add("Bloomington");
-		// places.add("NYC");
+//		 places.add("Indianapolis");
+//		 places.add("Terre Haute");
+//		 places.add("Bloomington");
+//		 places.add("NYC");
+	//	places.add()
 		Collections.sort(places);
 
 		new MapFrame();
@@ -164,8 +174,64 @@ public class MapFrame extends JFrame {
 	 *
 	 */
 	class Panel extends JPanel {
+		ArrayList<Node> nodes = new ArrayList<Node>();
+		public Panel(HashMap<String, String> cities) {
+			HashSet<String> keys = new HashSet<String>();
+			keys.addAll(cities.keySet());
+			for(String key : keys){
+				String[] str = cities.get(key).split(" ");
+				int x = Integer.parseInt(str[0]);
+				int y = Integer.parseInt(str[1]);
+				nodes.add(new Node(x,y,key));
+			}
+		}
 
+		public void paintComponent(Graphics comp) {
+			super.paintComponent(comp);
+			Graphics2D comp2D = (Graphics2D) comp;
+			for(int i = 0 ; i<nodes.size();i++){
+				nodes.get(i).drawOn(comp2D);
+			}
+		}
+		
+		class Node{// extends JComponent {
+			private double CenterX, CenterY;
+			private double radius;
+			private Color color;
+			private String value;
+
+			public Node(double x, double y, String v) {
+			//	this.setBorder(BorderFactory.createLoweredBevelBorder());
+				this.CenterX = x;
+				this.CenterY = y;
+				this.radius = 25;
+				this.color = new Color(255, 255, 0);
+				this.value = v;
+			}
+
+			public void drawOn(Graphics2D g2) {
+				Ellipse2D.Double cir = new Ellipse2D.Double(this.CenterX,
+						this.CenterY, this.radius, this.radius);
+				g2.draw(cir);
+				g2.setColor(this.color);
+				g2.fill(cir);
+				g2.setColor(Color.BLACK);
+				BasicStroke temp = new BasicStroke();
+				Shape outline = temp.createStrokedShape(cir);
+				g2.draw(outline);
+				g2.drawString("" + this.value, (float) (this.CenterX + 9.5),
+						(float) (this.CenterY + 16.5));
+			}
+
+			public void translate(double x, double y, String v) {
+				this.CenterX = x;
+				this.CenterY = y;
+				this.value = v;
+			}
+		}
 	}
+	
+	
 
 	/**
 	 * 
